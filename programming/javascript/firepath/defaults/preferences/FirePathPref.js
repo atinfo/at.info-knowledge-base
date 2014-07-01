aa94eb1f39d4d339a54b2e48fff6a2014b842325
sskuts <sergey.s.kuts@gmail.com>
2014-07-01 17:50:32 +0300

    FirePath update
    Added new feature to allow saving locators with name to a given file in
    java properties format.

---
+++ programming/javascript/firepath/defaults/preferences/FirePathPref.js
@@ -0,0 +1,4 @@
+pref("extensions.firebug.firepath.showParentToolbar", false);
+pref("extensions.firebug.firepath.generateAbsoluteXPath", false);
+pref("extensions.firebug.firepath.showDOM", true);
+pref("extensions.firebug.firepath.evaluationMode", "xpath");
