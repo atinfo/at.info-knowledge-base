79284c43e14e38b12aada43cc9555b5b19f4e0d4
Alexander Khotemskoy <Xotabu4@gmail.com>
2014-07-22 18:22:02 +0300

    Translated text RU->EN
    Translated text RU->EN;
    Updated description for path to uploaded file;

--- functional test automation/webdriver/file upload/upload_file.java
+++ functional test automation/webdriver/file upload/upload_file.java
@@ -7,23 +7,24 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 public class FileUpload {
 
 	static public void main(String[] args) {
-		// Создать экземпляр webdriver
+		// Creating webdriver
 		System.setProperty("webdriver.ie.driver", "C:\\WORK\\IEDriverServer_Win32_2.37.0\\IEDriverServer.exe");
 		WebDriver driver = new InternetExplorerDriver();
 
-	// Открыть страницу (в данном случае файл на локальной машине)
+	// Opening page. In this case - local HTML file.
     	driver.get("file://C:/WORK/test.html");
 
-    	// Найти на странице элемент для заливки файла
+    	// Find element that uploads file.
     	WebElement fileInput = driver.findElement(By.id("file"));
 
-    	// Указать элементу путь до файла (на диске)
+    	// Set direct path to local file that needs to be uploaded. 
+    	// That also can be a direct link to file in web, like - https://www.google.com.ua/images/srpr/logo11w.png
     	fileInput.sendKeys("file://C:/WORK/lenna.png");
 
-    	// Найти на странице кнопку отправки формы и нажать её
+    	// find button that sends form and click it.
     	driver.findElement(By.id("submit")).click();
 
-    	// Закрыть браузер и сессию webdriver
+    	// Closing driver and session
     	driver.quit();
 	}
 }
