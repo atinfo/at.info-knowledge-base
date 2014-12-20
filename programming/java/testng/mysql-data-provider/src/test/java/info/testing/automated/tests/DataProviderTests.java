30c99f3509915d9d1e1a43f55e48bac647461630
Sergey Kuts <sergey.s.kuts@gmail.com>
2014-12-20 22:17:10 +0200

    Removed redundant throw.

--- programming/java/testng/mysql-data-provider/src/test/java/info/testing/automated/tests/DataProviderTests.java
+++ programming/java/testng/mysql-data-provider/src/test/java/info/testing/automated/tests/DataProviderTests.java
@@ -47,7 +47,7 @@ public class DataProviderTests {
 
     @Entity(entity = Users.class, schema = AUTOMATION, ids = {1})
     @Test(dataProviderClass = DataProviderUtils.class, dataProvider = GENERIC_DP)
-    public void saveFirstSampleData(final Users user) throws InterruptedException {
+    public void saveFirstSampleData(final Users user) {
         user.setEmail("test.user1@email.com");
         user.setPassword("password1");
         user.save();
